1be0769464ad4f1fa4d2ca3c9c203c1656e3dcd6
zafer <48113419+riezaferdi@users.noreply.github.com>
2023-05-01 09:35:46 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 imgs=document.querySelectorAll('.result-card_image--art') 
 let txt = []; 
-for (var i = 0; i<imgs.length; i++){txt.push(imgs[i].src.replace('mobile-large', 'original'))} 
+for (var i = 0; i<imgs.length; i++){txt.push(imgs[i].src.replace('assets'))} 
 txt.toString().replace(/,/g, '\n')
